9ed20bbbb0cae81372b3f44154c0c9832b0122e5
andamagodwin <andamagodwinezra@gmail.com>
2025-07-25 09:51:31 +0300

    first commit

---
+++ build/bitnobgateway/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '711f4beef9eae09802ec');
---
+++ build/bitnobgateway/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '23922a3639f75611982e');
